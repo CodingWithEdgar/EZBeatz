7d481e63a8b0f04c05dde8f4ee553b2ff3e1fd2f
maldonado <edgarmaldonado709@gmail.com>
2021-04-14 21:32:49 -0400

    music class -> music function
    working

--- src/Music.js
+++ src/Music.js
@@ -1,88 +1,77 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import kick from './samples/trad_kick_01_C.wav'
 
-export default class Music extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            audioPlaying: false
+export default function Music(props) {
+    const [audioPlaying, setAudioPlaying] = useState(false);
+
+    const playAudio = () => {
+        setAudioPlaying(true);
+        const audioEl = document.getElementsByClassName("audio-element")[0];
+        audioEl.play();
     }
-    this.playAudio = this.playAudio.bind(this)
-    this.toggleAudio = this.toggleAudio.bind(this)
-    this.pauseAudio = this.pauseAudio.bind(this)
-    this.handleClick = this.handleClick.bind(this)
-    this.showControls = this.showControls.bind(this)
-}
-  playAudio() {
-    this.audioPlaying = true 
-    const audioEl = document.getElementsByClassName("audio-element")[0]
-    audioEl.play()
-  }
 
-  pauseAudio() {
-    this.audioPlaying = false
-    const audioEl = document.getElementsByClassName("audio-element")[0]
-    audioEl.pause()
-  }
+    const pauseAudio = () => {
+        setAudioPlaying(false);
+        const audioEl = document.getElementsByClassName("audio-element")[0];
+        audioEl.pause();
+    }
 
-  toggleAudio() {
-      if (!this.audioPlaying) {
-          this.playAudio();
-      } else {
-          this.pauseAudio();
-      }
-  }
+    const toggleAudio = () => {
+        if (!audioPlaying) {
+            playAudio();
+        } else {
+            pauseAudio();
+        }
+    }
+
+    const handleClick = () => {
+        toggleAudio();
+        showControls();
+    }
+
+    const showControls = () => {
+        return ( 
+            <div className="btn-group-vertical col">
+                <button type="button" className="btn btn-primary">
+                    Reverb
+                </button>
+                <button type="button" className="btn btn-success">
+                    Volume
+                <br></br>
+                <input type="range" />
+                </button>
+                <button type="button" className="btn btn-warning">
+                    Panning
+                <br></br>
+                <input type="range" />
+                </button>
+            </div>
+        )
+    }
   
-  showControls() {
-    return ( 
-        <div className="btn-group-vertical col">
-            <button type="button" className="btn btn-primary">
-                Reverb
-            </button>
-            <button type="button" className="btn btn-success">
-                Volume
-            <br></br>
-            <input type="range" />
-            </button>
-            <button type="button" className="btn btn-warning">
-                Panning
-            <br></br>
-            <input type="range" />
-            </button>
-        </div>
-    )
-  }
-
-  handleClick() {
-      this.toggleAudio();
-      this.showControls();
-  }
-
-  render() {
     return (
-      <div>
-        <svg onClick={this.handleClick} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <circle fill={this.props.color} cx="10" cy="10" r="5" ></circle>
+    <div>
+        <svg onClick={handleClick} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <circle fill={props.color} cx="10" cy="10" r="5" ></circle>
         </svg>
         <audio loop className="audio-element">
-          <source src={kick}></source>
+        <source src={kick}></source>
         </audio>
         <button type="button" className="btn btn-primary">
-              Reverb
-              <br></br>
-              <input type="range" />
+            Reverb
+            <br></br>
+            <input type="range" />
             </button>
             <button type="button" className="btn btn-success">
-              Volume
-              <br></br>
-              <input type="range" />
+            Volume
+            <br></br>
+            <input type="range" />
             </button>
             <button type="button" className="btn btn-warning">
-              Panning
-              <br></br>
-              <input type="range" />
+            Panning
+            <br></br>
+            <input type="range" />
             </button>
-      </div>
-    )
-  }
+    </div>
+    );
 }
