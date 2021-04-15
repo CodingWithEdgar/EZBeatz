2bb52d785783596134f97e8d29caccdc38752343
MI <morel.m@husky.neu.edu>
2021-04-15 00:02:39 -0400

    adding filters

--- src/Music.js
+++ src/Music.js
@@ -1,81 +1,79 @@
-import React, { useState } from "react";
-import kick from './samples/trad_kick_01_C.wav'
+import React, {useEffect, useState} from "react";
 
 export default function Music(props) {
-<<<<<<< Updated upstream
-    const [audioPlaying, setAudioPlaying] = useState(false);
-=======
-  const AudioContext = new (window.AudioContext || window.webkitAudioContext)();
-  const audioCtx = new AudioContext();
-
   const audioTune = new Audio(props.audio);
-
-  const track = audioCtx.createMediaElementSource(audioTune);
-  track.connect(audioCtx.destination);
-
-  gainNode = this.audioCtx.createGain();
-  gainNode.connect(audioCtx.destination);
-  gainNode.gain.value =  0.5;
-
-
-
-
-
-  const = audioCtx.createMediaElementSource(this.audioElement);
-
 
   const[playing, setPlaying] = useState(false)
   const [playInLoop, setPlayInLoop] = useState(true);
   const [_showControls, _setShowControls] = useState(false);
->>>>>>> Stashed changes
 
-    const playAudio = () => {
-        setAudioPlaying(true);
-        const audioEl = document.getElementsByClassName("audio-element")[0];
-        audioEl.play();
-    }
 
-    const pauseAudio = () => {
-        setAudioPlaying(false);
-        const audioEl = document.getElementsByClassName("audio-element")[0];
-        audioEl.pause();
-    }
 
-    const toggleAudio = () => {
-        if (!audioPlaying) {
-            playAudio();
-        } else {
-            pauseAudio();
-        }
-    }
 
-    const handleClick = () => {
-        toggleAudio();
-        showControls();
-    }
+  // load audio file on component load
+  useEffect(() => {
+      audioTune.load();
+  }, [audioTune])
 
-    const showControls = () => {
-        return ( 
-            <div className="btn-group-vertical col">
-                <button type="button" className="btn btn-primary">
-                    Reverb
-                </button>
-                <button type="button" className="btn btn-success">
-                    Volume
-                <br></br>
-                <input type="range" />
-                </button>
-                <button type="button" className="btn btn-warning">
-                    Panning
-                <br></br>
-                <input type="range" />
-                </button>
-            </div>
-        )
+  // set the loop of audio tune
+  useEffect(() => {
+    audioTune.loop = playInLoop;
+  }, [playInLoop, audioTune])
+
+
+  const playAudio = () => {
+    console.log("play")
+    setPlaying(true);
+    audioTune.play();
+  };
+
+  const stopAudio = () => {
+    setPlaying(false);
+    setPlayInLoop(false);
+    audioTune.pause();
+    audioTune.currentTime = 0;
+    console.log("pause")
+  };
+
+  const createContext = () =>{
+    var AudioContext = (window.AudioContext || window.webkitAudioContext);
+    const audioCtx = new AudioContext();
+
+    const track = audioCtx.createMediaElementSource(audioTune);
+    track.connect(audioCtx.destination);
+
+    const volume = audioCtx.createGain();
+    volume.connect(audioCtx.destination);
+    volume.gain.value =  0.5;
+
+
+    const panner = new StereoPannerNode(audioCtx, {pan: 0});
+    panner.connect(audioCtx.destination);
+
+    //reverb
+    //https://middleearmedia.com/web-audio-api-convolver-node/
+    var convolver = audioCtx.createConvolver();
+
+
+    //filter
+    let bandHz = 5000;
+    const filter = audioCtx.createBiquadFilter();
+    filter.type = 'lowpass';
+    filter.frequency.value = bandHz;
+    filter.connect(audioCtx.destination);
+
+
+    audioTune.play()
+    //track.start();
+  };
+
+  const toggleAudio = () => {
+    if (!playing) {
+      playAudio();
+      createContext();
+    } else {
+      stopAudio();
     }
-<<<<<<< Updated upstream
-  
-=======
   };
 
   const toggleControls = () => {
@@ -88,30 +86,34 @@ export default function Music(props) {
   };
 
   const showControls = () => {
->>>>>>> Stashed changes
     return (
-    <div>
-        <svg onClick={handleClick} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <circle fill={props.color} cx="10" cy="10" r="5" ></circle>
-        </svg>
-        <audio loop className="audio-element">
-        <source src={kick}></source>
-        </audio>
+      <div className="btn-group-vertical col">
         <button type="button" className="btn btn-primary">
-            Reverb
-            <br></br>
-            <input type="range" />
-            </button>
-            <button type="button" className="btn btn-success">
-            Volume
-            <br></br>
-            <input type="range" />
-            </button>
-            <button type="button" className="btn btn-warning">
-            Panning
-            <br></br>
-            <input type="range" />
-            </button>
-    </div>
+          Reverb
+        </button>
+        <button type="button" className="btn btn-success">
+          Volume
+          <br></br>
+          <input type="range" />
+        </button>
+        <button type="button" className="btn btn-warning">
+          Panning
+          <br></br>
+          <input type="range" />
+        </button>
+      </div>
     );
+  };
+
+  return (
+    <div>
+      <svg
+        onClick={handleClick}
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg">
+        <circle fill={props.color} cx="10" cy="10" r="5"></circle>
+      </svg>
+      {_showControls ? showControls() : null}
+    </div>
+  );
 }
