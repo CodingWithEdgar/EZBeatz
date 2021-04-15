9a244a4c35b609f1e371f78586bc3c44e4016ecc
maldonado <edgarmaldonado709@gmail.com>
2021-04-14 21:11:28 -0400

    Update Music.js
    add controls to working sound circles

--- src/Music.js
+++ src/Music.js
@@ -5,74 +5,84 @@ export default class Music extends Component {
     constructor(props) {
         super(props);
         this.state = {
-        audioPlaying: false,
-    };
-    this.playAudio = this.playAudio.bind(this);
-    this.toggleAudio = this.toggleAudio.bind(this);
-    this.pauseAudio = this.pauseAudio.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.showControls = this.showControls.bind(this);
-  }
-
+            audioPlaying: false
+    }
+    this.playAudio = this.playAudio.bind(this)
+    this.toggleAudio = this.toggleAudio.bind(this)
+    this.pauseAudio = this.pauseAudio.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.showControls = this.showControls.bind(this)
+}
   playAudio() {
-    this.audioPlaying = true;
-    const audioEl = document.getElementsByClassName("audio-element")[0];
-    audioEl.play();
+    this.audioPlaying = true 
+    const audioEl = document.getElementsByClassName("audio-element")[0]
+    audioEl.play()
   }
 
   pauseAudio() {
-    this.audioPlaying = false;
-    const audioEl = document.getElementsByClassName("audio-element")[0];
-    audioEl.pause();
+    this.audioPlaying = false
+    const audioEl = document.getElementsByClassName("audio-element")[0]
+    audioEl.pause()
   }
 
   toggleAudio() {
-    if (!this.audioPlaying) {
-      this.playAudio();
-    } else {
-      this.pauseAudio();
-    }
+      if (!this.audioPlaying) {
+          this.playAudio();
+      } else {
+          this.pauseAudio();
+      }
   }
-
+  
   showControls() {
-    // wrong bc I need to flip a var then check the var local to this component instance
-    return (
-      <div className="btn-group-vertical col">
-        <button type="button" className="btn btn-primary">
-          Reverb
-        </button>
-        <button type="button" className="btn btn-success">
-          Volume
-          <br></br>
-          <input type="range" />
-        </button>
-        <button type="button" className="btn btn-warning">
-          Panning
-          <br></br>
-          <input type="range" />
-        </button>
-      </div>
-    );
+    return ( 
+        <div className="btn-group-vertical col">
+            <button type="button" className="btn btn-primary">
+                Reverb
+            </button>
+            <button type="button" className="btn btn-success">
+                Volume
+            <br></br>
+            <input type="range" />
+            </button>
+            <button type="button" className="btn btn-warning">
+                Panning
+            <br></br>
+            <input type="range" />
+            </button>
+        </div>
+    )
   }
 
   handleClick() {
-    this.toggleAudio();
+      this.toggleAudio();
+      this.showControls();
   }
 
   render() {
     return (
       <div>
-        <svg
-          onClick={this.handleClick}
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle fill={this.props.color} cx="10" cy="10" r="5"></circle>
+        <svg onClick={this.handleClick} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <circle fill={this.props.color} cx="10" cy="10" r="5" ></circle>
         </svg>
         <audio loop className="audio-element">
           <source src={kick}></source>
         </audio>
+        <button type="button" className="btn btn-primary">
+              Reverb
+              <br></br>
+              <input type="range" />
+            </button>
+            <button type="button" className="btn btn-success">
+              Volume
+              <br></br>
+              <input type="range" />
+            </button>
+            <button type="button" className="btn btn-warning">
+              Panning
+              <br></br>
+              <input type="range" />
+            </button>
       </div>
-    );
+    )
   }
 }
